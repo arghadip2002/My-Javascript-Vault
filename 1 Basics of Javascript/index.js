db6d23bf7572a0955461a6a3b62a9822e2335378
Arghadip Biswas <arghadipbiswas@rkmgec.ac.in>
2025-04-23 18:52:47 +0530

    Created the Javascript vault repo, and added nitial code files and folders

---
+++ 1 Basics of Javascript/index.js
@@ -0,0 +1,8 @@
+console.log(`Hello`);
+
+window.alert(`I like Pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza`;
+
+// comments
---
+++ 3 Arithmatic Operators/index.js
@@ -0,0 +1,27 @@
+let age = 25;
+let price = 10;
+let gpa = 9.5;
+
+console.log(`My age is ${age}`);
+console.log(`My pocket money is $${price}`);
+console.log(`My gpa is ${gpa}`);
+
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof gpa);
+
+let firstName = "Bro";
+console.log(`My name is ${firstName}`);
+console.log(typeof firstName);
+
+let online = true;
+console.log(typeof online);
+console.log(`${firstName} is online : ${online}`);
+
+let fullName = "Arghadip Biswas";
+let ageInYears = 22;
+let student = false;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = `My age is ${ageInYears}`;
+document.getElementById("p3").textContent = `I am a Student : ${student}`;
